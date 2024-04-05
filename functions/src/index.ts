c76e932c2653b0031f820eeef64d6665b02a25c1
Mink <lienhung75757575@gmail.com>
2024-04-05 22:07:32 +0700

    add cloud function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,10 +1,32 @@
-import * as v1 from 'firebase-functions/v1';
+
+import * as admin from 'firebase-admin';
+import { Timestamp } from 'firebase-admin/firestore';
+import * as functions from 'firebase-functions';
+import { v4 as uuidv4 } from 'uuid';
+
+admin.initializeApp();
+
+const db = admin.firestore();
 
 
+export const onCreatePost = functions.firestore
+    .document('posts/{postId}')
+    .onCreate(async (snapshot, context) => {
+        const postData = snapshot.data();
+        const usersRef = db.collection('users');
+        const querySnapshot = await usersRef.where('uid', '==', postData.uid).get();
+        querySnapshot.forEach(async (doc) => {
+          const follower = doc.get('followers');
+          const userName = doc.get('username');
+          for(var i = 0; i < follower.length; i++) {
+            const notificationsId: string = uuidv4();
+            await db.doc(`users/${follower[i]}/notifications/${notificationsId}`)
+              .set({
+                notificationsId: notificationsId, 
+                content: `${userName} just upload a new post`, 
+                Timestamp: Timestamp.now()
+              });
+          }
+        });
+    });
 
-export const newsku = v1.firestore.document('news/{newsId}').onCreate((snap, context) => {
-  const newValue = snap.data();
-  console.log('new sku', newValue);
-  return null;
-});
-```
