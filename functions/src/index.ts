d2f063a49a6b47fb13427c4a0c517a65706da835
Mink <lienhung75757575@gmail.com>
2024-04-07 19:20:42 +0700

    update cloud function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -7,26 +7,52 @@ import { v4 as uuidv4 } from 'uuid';
 admin.initializeApp();
 
 const db = admin.firestore();
+const fcm = admin.messaging();
 
 
-export const onCreatePost = functions.firestore
-    .document('posts/{postId}')
-    .onCreate(async (snapshot, context) => {
-        const postData = snapshot.data();
-        const usersRef = db.collection('users');
-        const querySnapshot = await usersRef.where('uid', '==', postData.uid).get();
-        querySnapshot.forEach(async (doc) => {
-          const follower = doc.get('followers');
-          const userName = doc.get('username');
-          for(var i = 0; i < follower.length; i++) {
-            const notificationsId: string = uuidv4();
-            await db.doc(`users/${follower[i]}/notifications/${notificationsId}`)
-              .set({
-                notificationsId: notificationsId, 
-                content: `${userName} just upload a new post`, 
-                Timestamp: Timestamp.now()
-              });
-          }
-        });
+export const onPostCreate = functions.firestore
+  .document('posts/{postId}')
+  .onCreate(async (snapshot, context) => {
+    const postData = snapshot.data();
+    const usersRef = db.collection('users');
+    const querySnapshot = await usersRef.where('uid', '==', postData.uid).get();
+    querySnapshot.forEach(async (doc) => {
+      const follower = doc.get('followers');
+      const userName = doc.get('username');
+      for (var i = 0; i < follower.length; i++) {
+        const notificationsId: string = uuidv4();
+        await db.doc(`users/${follower[i]}/notifications/${notificationsId}`)
+          .set({
+            notificationsId: notificationsId,
+            postId: postData.postId,
+            content: `${userName} just upload a new post`,
+            Timestamp: Timestamp.now()
+          });
+      }
     });
+  });
+
+export const onNotificationCreate = functions.firestore
+  .document('users/{userId}/notifications/{notificationsId}')
+  .onCreate(async (snapshot, context) => {
+    const userId = context.params.userId;
+    const content = snapshot.get('content');
+    const postId = snapshot.get('postId');
+    const tokenSnapshot = await db.collection('users').doc(userId).get();
+    const token = tokenSnapshot.data()?.fcmToken;
+
+    const message: admin.messaging.Message = {
+      token: token,
+      notification: {
+        title: 'New Post',
+        body: content,
+      },
+      data: {
+        postId: postId,
+        uid: userId,
+      },
+    };
+    fcm.send(message);
+  });
+
 
