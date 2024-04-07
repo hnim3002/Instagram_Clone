2a6fc38618f6e2cb6cfa60e878790379229c8c92
Mink <lienhung75757575@gmail.com>
2024-04-07 19:19:52 +0700

    add navigatorKey

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:instagram_clon/providers/comments_provider.dart';
@@ -7,6 +9,7 @@ import 'package:instagram_clon/providers/comments_state_provider.dart';
 import 'package:instagram_clon/providers/posts_provider.dart';
 import 'package:instagram_clon/providers/posts_state_provider.dart';
 import 'package:instagram_clon/providers/user_provider.dart';
+import 'package:instagram_clon/resources/messaging_method.dart';
 import 'package:instagram_clon/responsive/mobile_screen.dart';
 import 'package:instagram_clon/responsive/responsive_layout.dart';
 import 'package:instagram_clon/responsive/web_screen.dart';
@@ -17,12 +20,30 @@ import 'package:provider/provider.dart';
 
 import 'firebase_options.dart';
 
+const bool USE_EMULATOR = true;
+
+final navigatorKey = GlobalKey<NavigatorState>();
+
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+
+  if (USE_EMULATOR) {
+    await useEmulator();
+  }
   runApp(const MyApp());
+}
+
+Future<void> useEmulator() async {
+  const localHostString = "10.0.2.2";
+
+  FirebaseFirestore.instance.useFirestoreEmulator(localHostString, 8080);
+
+  await FirebaseAuth.instance.useAuthEmulator(localHostString, 9099);
+
+  await FirebaseStorage.instance.useStorageEmulator(localHostString, 9199);
 }
 
 class MyApp extends StatelessWidget {
@@ -35,57 +56,56 @@ class MyApp extends StatelessWidget {
       systemNavigationBarColor:
           Colors.white, // set color of system navigation bar
       statusBarColor: Colors.white,
-      statusBarIconBrightness: Brightness.dark, // Set the status bar text color// transparent status bar
+      statusBarIconBrightness: Brightness
+          .dark, // Set the status bar text color// transparent status bar
     ));
     return MultiProvider(
       providers: [
         ChangeNotifierProvider(
           create: (_) => UserProvider(),
         ),
-        ChangeNotifierProvider(
-          create: (_) => CommentsStateProvider()
-        ),
-        ChangeNotifierProvider(
-            create: (_) => CommentsProvider()
-        ),
-        ChangeNotifierProvider(
-            create: (_) => PostsProvider()
-        ),
-        ChangeNotifierProvider(
-            create: (_) => PostsStateProvider()
-        )
+        ChangeNotifierProvider(create: (_) => CommentsStateProvider()),
+        ChangeNotifierProvider(create: (_) => CommentsProvider()),
+        ChangeNotifierProvider(create: (_) => PostsProvider()),
+        ChangeNotifierProvider(create: (_) => PostsStateProvider())
       ],
       child: MaterialApp(
           title: 'Flutter Demo',
           theme: ThemeData.light(useMaterial3: true).copyWith(
             scaffoldBackgroundColor: Colors.white,
           ),
+          navigatorKey: navigatorKey,
           darkTheme: ThemeData.dark(useMaterial3: true)
               .copyWith(scaffoldBackgroundColor: Colors.black),
           routes: {
             '/home_screen': (context) => const MobileScreenLayout(),
           },
-
           home: StreamBuilder(
               stream: FirebaseAuth.instance.authStateChanges(),
               builder: (context, snapshot) {
-                if (snapshot.hasData) {
-                  return const ResponsiveLayout(
-                    mobileScreenLayout: MobileScreenLayout(),
-                    webScreenLayout: WebScreenLayout(),
-                  );
-                } else if (snapshot.hasError) {
-                  return Center(
-                    child: Text("${snapshot.hasError}"),
-                  );
-                }
                 if (snapshot.connectionState == ConnectionState.waiting) {
                   return const Center(
                     child: CircularProgressIndicator(),
                   );
                 }
-                return const LoginScreen();
-              })),
+                if (snapshot.hasError) {
+                  return Center(
+                    child: Text("${snapshot.error}"),
+                  );
+                }
+                if (snapshot.hasData && snapshot.data != null) {
+                  MessagingMethod().uploadTokenToServer();
+                  MessagingMethod().initNotifications();
+                  return const ResponsiveLayout(
+                    mobileScreenLayout: MobileScreenLayout(),
+                    webScreenLayout: WebScreenLayout(),
+                  );
+                } else {
+                  // No user authenticated
+                  return const LoginScreen();
+                }
+              }
+          )),
     );
   }
 }
